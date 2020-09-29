d41cfe53e3a6f1e52de6d66756969cf89dbf14f8
phongvilai noypha <phongvilaitoto@gmail.com>
2020-09-29 14:03:21 +0700

    change url

--- src/config/variables.js
+++ src/config/variables.js
@@ -2,4 +2,4 @@
 
 const isProd = process.env.NODE_ENV === 'production' ? true : false
 
-export const baseURL = isProd ? 'https://108.laofix.com' : 'http://localhost:4000'
+export const baseURL = isProd ? 'https://payroll.laofix.com' : 'http://localhost:4000'
