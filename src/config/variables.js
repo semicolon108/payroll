985ac2ae48c701737eed7121e8c6a89eea19be81
phongvilai noypha <phongvilaitoto@gmail.com>
2020-09-29 13:36:08 +0700

    show

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,1 +1,5 @@
-export const baseURL = 'http://localhost:4000'
+
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false
+
+export const baseURL = isProd ? 'https://108.laofix.com' : 'http://localhost:4000'
