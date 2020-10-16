6b5cbffcd2e619e6f138f05a122877e7f446e4d8
phongvilai noypha <phongvilaitoto@gmail.com>
2020-10-16 08:41:49 +0700

    hi

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,10 +1,8 @@
 
-
 const isProd = process.env.NODE_ENV === 'production' && true
 
 export const loadingTimeout = 600
 
 export const logoutURL = isProd ? 'https://auth-payroll.sabai.la' : 'http://localhost:3000'
-
 
 export const baseURL = isProd ? 'https://payroll.sabai.la' : 'http://localhost:4000'
