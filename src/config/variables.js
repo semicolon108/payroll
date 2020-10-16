d0e28dd6155ece2034981c560a3bf13a1ec8ed61
phongvilai noypha <phongvilaitoto@gmail.com>
2020-10-16 08:34:05 +0700

    change url

--- src/config/variables.js
+++ src/config/variables.js
@@ -6,4 +6,5 @@ export const loadingTimeout = 600
 
 export const logoutURL = isProd ? 'https://auth-payroll.sabai.la' : 'http://localhost:3000'
 
+
 export const baseURL = isProd ? 'https://payroll.sabai.la' : 'http://localhost:4000'
