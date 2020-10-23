a887169567fa01f985de1f43257b75348adb9943
phongvilai noypha <phongvilaitoto@gmail.com>
2020-10-23 16:32:10 +0700

    update opc

--- src/components/Confirm/Child.vue
+++ src/components/Confirm/Child.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="dialog-bg"
+         style="opacity: 30%"
          v-if="item.isShow"></div>
     <transition name="bounce">
       <div v-if="item.isShow && item.type === 'Alert'" class="flex-container">
