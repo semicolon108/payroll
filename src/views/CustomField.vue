35acf277c8fd211f83c0f96d75f10a9d7603d8d9
phongvilai noypha <phongvilaitoto@gmail.com>
2021-03-10 14:03:45 +0700

    ud custom fields

--- src/views/CustomField.vue
+++ src/views/CustomField.vue
@@ -10,8 +10,16 @@
             <p>Need help or have questions about uploading document? Call us at (856) 21 254709.</p>
         </div>
         <div class="flex flex-wrap">
-            <div class="w-1/2 p-2" v-for="(i, idx) in fields" :key="idx" >
+            <div class="w-1/2 p-2 relative" v-for="(i, idx) in fields" :key="idx" >
                 <input v-model="fields[idx]" type="text input " style="width: 100%; height: 40px" class="p-2">
+                 <span  
+
+                 @click="fields.splice(idx, 1)"
+                 style="position: absolute;  right: 0; top: 0; margin-right: 10px; margin-top: 15px;"
+                 class="icon alert 
+
+                 cursor-pointer
+                 text-red-500"><i class="fas fa-trash"></i></span>
             </div>
         </div>
         <div class="p-2">
