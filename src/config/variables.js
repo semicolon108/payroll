563ac1cbed8002667c0e81731bc7e7d2a44e4018
phongvilai noypha <phongvilaitoto@gmail.com>
2021-07-22 14:42:08 +0700

    change NODE_ENV

--- src/config/variables.js
+++ src/config/variables.js
@@ -4,7 +4,10 @@
 
 export const loadingTimeout = 100
 
-export const logoutURL = isProd ? 'http://auth.hr-sabai.com' : 'http://localhost:3000'
+export const logoutURL = 'http://auth.hr-sabai.com'
+//isProd ? 'http://auth.hr-sabai.com' : 'http://localhost:3000'
 
-export const baseURL = isProd ? 'https://hr-sabai.com' : 'http://localhost:4000'
+export const baseURL = 'https://hr-sabai.com'
+
+//isProd ? 'https://hr-sabai.com' : 'http://localhost:4000'
 
