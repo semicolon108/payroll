4ccdd58378d19c086732bc3e7032185d94fe8d74
luangkhot <anousone108@me.com>
2021-07-26 11:05:45 +0700

    payslip template page

--- src/views/DocumentTemplate.vue
+++ src/views/DocumentTemplate.vue
@@ -1,63 +1,67 @@
 <template>
-    <div>
-        <div class="page-header">
-            <h3 class="page-title">Payslip Template</h3>
-            <!-- <button class="button primary">Add Template</button> -->
-        </div>
-        <div class="flex">
-            <div class="shadow w-1/2 m-2 relative"
-             :class="{'border border-blue-600': !payslipTemplate.isDefault}">
-                <div class="m-3">
-                    <h2 class="font-bold"
-                    :class="{'text-blue-800': !payslipTemplate.isDefault}"
-                    >Default Template</h2>
-                    <p class="text-gray-400 text-xs mt-2">This option is chosen by default (You can't modify it).</p>
-                </div>
-                <button class="border-r border-t p-2 "
-                :class="{'bg-blue-800 text-white': !payslipTemplate.isDefault}"
+<div>
+    <div class="page-header">
+        <h3 class="page-title">Payslip Template</h3>
+        <!-- <button class="button primary">Add Template</button> -->
+    </div>
+    <div class="columns">
+        <div class="column is-4-desktop">
+            <div class="box" :class="{'primary': !payslipTemplate.isDefault}">
+
+                <h3  class="title" :class="{'primary': !payslipTemplate.isDefault}">Default Template</h3>
+
+                <button class="button"
+                :class="{'primary': !payslipTemplate.isDefault}"
                 @click="setDefault(false)"
                 >Set Default</button>
-                <div class="absolute right-0 top-0 mt-4 mr-4  text-blue-800" >
+
+                <div class="download">
                     <a download href="https://paytoday-bucket.s3-ap-southeast-1.amazonaws.com/payslip-templates/DefaultTemplate.docx">
-                    <i class="fas fa-lg  fa-file-download cursor-pointer"></i></a>
-                </div>
-            </div>
-               <div class="shadow w-1/2 m-2 relative"
-               :class="{'border border-blue-600': payslipTemplate.isDefault}"
-               >
-                <div class="m-3">
-                    <h2 class="font-bold"
-                     :class="{'text-blue-800': payslipTemplate.isDefault}"
-                    >Custom Template 
-                       <span v-if="payslipTemplate._id" class="text-green-400">(READY TO USE)</span>
-                     <span v-else class="text-red-400">(NOT READY)</span>
-                  
-                    </h2>
-                    <p class="text-gray-400 text-xs mt-2">You can change what you want.</p>
-                </div>
-                <button 
-                v-if="payslipTemplate._id"
-                class="border-r border-t p-2 "
-                 :class="{'bg-blue-800 text-white': payslipTemplate.isDefault}"
-                 @click="setDefault(true)"
-                
-                >Set Default</button>
-                  <div class="absolute right-0 top-0 mt-4 mr-4 text-blue-800 text-right" >
-                    <a v-if="payslipTemplate.payslip" download :href="payslipTemplate.payslip.src">
-                        <i class="block fa-lg fas fa-file-download cursor-pointer"></i>
+                        <i class="fal fa-file-word"></i>
                     </a>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="column is-4-desktop">
+            <div class="box" :class="{'primary': payslipTemplate.isDefault}">     
+                <h3 class="title"
+                    :class="{'primary': payslipTemplate.isDefault}"
+                    >Custom Template 
+                    <span v-if="payslipTemplate._id" class="primary">(READY TO USE)</span>
+                    <span v-else class="">(NOT READY)</span>
+                </h3>
+
+                <div class="buttons">
                     <button 
-                    @click="$refs.fileInput.click()"
-                    class="bg-gray-400 text-white px-2 py-1 mt-4">Upload template</button>
-                    <p v-if="payslipTemplate._id" class="text-xs mt-1 text-green-400">Template Size: ({{payslipTemplate.payslip.size | fileSize}})</p>
+                    v-if="payslipTemplate._id"
+                    class="button"
+                    :class="{'primary': payslipTemplate.isDefault}"
+                    @click="setDefault(true)"
+                    >Set Default</button>
+
+
+                    <div class="upload">
+                        <button @click="$refs.fileInput.click()" class="button">Upload template</button>
+                        <p v-if="payslipTemplate._id" class="text-xs mt-1 text-green-400">Template Size: ({{payslipTemplate.payslip.size | fileSize}})</p>
+                    </div>
+                </div>
+
+                <div class="download">
+                    <a v-if="payslipTemplate.payslip" download :href="payslipTemplate.payslip.src">
+                        <i class="fal fa-file-word"></i>
+                    </a>
                 </div>
             </div>
         </div>
-        <input 
-        ref="fileInput"
-        @change="onFileChange"
-        type="file" class="hidden">
     </div>
+    <input 
+    ref="fileInput"
+    @change="onFileChange"
+    type="file" class="is-hidden">
+</div>
 </template>
 
 <script>
@@ -116,5 +120,53 @@ import {getPayslipTemplateApi, addOrUpdatePayslipTemplateApi} from '@/apis/paysl
 </script>
 
 <style lang="scss" scoped>
+.box{
+    position: relative;
+    transition: ease .2s;
+    &.primary{
+        box-shadow: 0 0 0 1px $primary-color;
+    }
+}
+.title{
+    font-size: $desktop-subtitle;
+    margin-bottom: 20px;
+    &.primary{
+        color: $primary-color;
+    }
+    span{
+        color: $grey-color;
+        &.primary{
+            color: $sub-color;
+        }
+    }
+}
+.buttons{
+    display: flex;
+    align-items: center;
+    margin-bottom: 0;
+    button{
+        margin-right: 5px;
+        margin-bottom: 0;
+    }
+    .upload{
+        display: flex;
+        align-items: center;
+        p{
+            margin: 0;
+            font-size: 14px;
+        }
+    }
+}
 
+.download{
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 5px 15px;
+    background-color: $light-grey-color;
+    font-size: 16px;
+    a{
+        color: #000;
+    }
+}
 </style>
