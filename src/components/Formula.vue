f5bfc6cfa8985eecf37cead7dde9736e66573a04
phongvilai noypha <phongvilaitoto@gmail.com>
2022-09-05 14:33:30 +0700

    add field thisMonthSalary

--- src/components/Formula.vue
+++ src/components/Formula.vue
@@ -188,6 +188,7 @@ export default {
         symbols: ["+", "-", "*", "/"],
         keys: [
             "basicSalary",
+            'thisMonthSalary',
             "tax",
             "ssoPaidByEmp",
             // "finalNetPay",
